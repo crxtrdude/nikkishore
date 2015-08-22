bedf14dae4613563e08c81b3b77851c7e5ba8175
CRxTRDude <crxtrtupgm@gmail.com>
2015-08-22 21:04:03 +0800

    Major revision:
    + Added new textures from FreeDoom as well as made by me.
    + Added some placeholder textures
    + Added a new system for enemies, with pro as the new enemy class.
    - New expanded NE1M1
    - Edited Stunner.
    - Modified ALTHUD alignments and new sprites
    - Modified pk3 compiler list
    x Removed ZKVN sources, but added compiled binaries on ACS.
    
    NOTE: nikkifist needs to be modified.
    TODO: modify intermission stuff.

--- Oven.bat
+++ Oven.bat
@@ -1,10 +1,23 @@
 @ECHO OFF
 
+:choice
+@ECHO Do you want to compile the pk3 with music?
+set /P c=[Y/N]?
+if /I "%c%" EQU "Y" goto :withmusic
+if /I "%c%" EQU "N" goto :nomusic
+goto :choice
+
+:withmusic
 @ECHO.
 @ECHO Now baking NikkiShore.pk3
 @ECHO.
+tools\7za\7za.exe a -tzip bin\nikkishore.pk3 @listfile.txt
 
-tools\7za\7za.exe a -tzip bin\nikkishore.pk3 @listfile.txt >nul
+:nomusic
+@ECHO.
+@ECHO Now baking NikkiShore.pk3
+@ECHO.
+tools\7za\7za.exe a -tzip bin\nikkishore.pk3 @listfile_nomusic.txt
 
 @ECHO.
 @ECHO Finished baking NikkiShore.pk3
