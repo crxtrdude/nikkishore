652cc09f34ee48ae60594af72399672248de1336
CRxTRDude <crxtrtupgm@gmail.com>
2015-09-18 22:14:26 +0800

    Finally! New objective system!
    + New objective system with 'clue' support. Pick up clues and you can store it on the notebook, which you could cycle over to read.
    + New handwritten fonts!
    - Updated compileacs a bit
    - Added potted plant on the statics includes since I didn't added it (it was committed ahead)
    - Fixed Randombasic. Replaced the fatso name to fatdude, as there was already a fatso enemy in Hexen.

--- compileacs.bat
+++ compileacs.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+:compile
+cls
 @ECHO.
 @ECHO Cleaning ACS folder
 @ECHO.
@@ -14,6 +16,23 @@ acc.exe -i ".." ..\..\carcontrol.acs ..\..\acs\carcontrol.o
 acc.exe -i ".." ..\..\qcommon.acs ..\..\acs\qcommon.o
 acc.exe -i ".." ..\..\qmsg.acs ..\..\acs\qmsg.o
 acc.exe -i ".." ..\..\qdialog.acs ..\..\acs\qdialog.o
+acc.exe -i ".." ..\..\qobjectives.acs ..\..\acs\qobjectives.o
 @ECHO.
 @ECHO Scripts Finished.
+pause
+
+:choice
+cls
+@ECHO.
+@ECHO Do you want to compile again?
+set /P c=[Y/N]?
+if /I "%c%" EQU "Y" goto :compile
+if /I "%c%" EQU "N" goto :exit
+goto :choice
+
+:exit
+cls
+@ECHO.
+@ECHO Version 1.1
+@ECHO.
 pause
